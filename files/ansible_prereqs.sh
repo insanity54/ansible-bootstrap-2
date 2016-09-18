a6620cde36df5cd09049cadfb7f1e681ef18664c
Chris Grimmett <chris@grimtech.net>
2016-09-18 13:31:23 -0700

    remove python

--- files/ansible_prereqs.sh
+++ files/ansible_prereqs.sh
@@ -3,7 +3,7 @@
 # http://euphonious-intuition.com/2013/01/bootstrapping-a-cluster-with-ansible-debian-6-and-oracle-java-7/
 if [ ! -f /root/.ansible_prereqs_installed ]; then
         apt-get update
-        apt-get install -y python python-apt python-pycurl sshpass
+        apt-get install -y sshpass
         touch /root/.ansible_prereqs_installed
         echo "CHANGE"
 fi

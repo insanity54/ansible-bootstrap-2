9954da57de6d74c0698cec0c1bfe41fe977b4ac8
Chris Grimmett <chris@grimtech.net>
2016-09-18 13:41:43 -0700

    add build-essential

--- files/ansible_prereqs.sh
+++ files/ansible_prereqs.sh
@@ -3,7 +3,7 @@
 # http://euphonious-intuition.com/2013/01/bootstrapping-a-cluster-with-ansible-debian-6-and-oracle-java-7/
 if [ ! -f /root/.ansible_prereqs_installed ]; then
         apt-get update
-        apt-get install -y sshpass
+        apt-get install -y sshpass build-essential
         touch /root/.ansible_prereqs_installed
         echo "CHANGE"
 fi
